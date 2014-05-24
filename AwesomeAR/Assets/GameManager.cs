514f654ad272a7e7bf37004bce26a2e8695e830f
jkwendorf <jkwendorf>
2014-05-24 09:16:14 -0700

    Added project

---
+++ AwesomeAR/Assets/GameManager.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	public GameObject O_1;
+	public GameObject O_2;
+	public GameObject O_3;
+	public GameObject O_4;
+	public GameObject O_5;
+	public GameObject O_6;
+	public GameObject O_7;
+	public GameObject O_8;
+	public GameObject O_9;
+	public GameObject X_1;
+	public GameObject X_2;
+	public GameObject X_3;
+	public GameObject X_4;
+	public GameObject X_5;
+	public GameObject X_6;
+	public GameObject X_7;
+	public GameObject X_8;
+	public GameObject X_9;
+
+	public Font gameFont;
+	GUIStyle gameStyle;
+	int player = 0;
+	string message;
+
+	bool set1, set2, set3, set4, set5, set6, set7, set8, set9;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+		player = 1;
+		set1 = false;
+		set2 = false;
+		set3 = false;
+		set4 = false;
+		set5 = false;
+		set6 = false;
+		set7 = false;
+		set8 = false;
+		set9 = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		message = "It's player " + player + "'s turn";
+	}
+
+	void OnGUI()
+	{
+		gameStyle = new GUIStyle();
+		gameStyle.font = gameFont;
+		gameStyle.normal.textColor = Color.white;
+
+		GUIUtility.RotateAroundPivot(90, new Vector2(1100, 0));
+
+		GUI.Label(new Rect(1100, 0, 300, 50), message, gameStyle);
+	}
+
+	// Switches turns for the player
+	void ChangeTurn()
+	{
+		player++;
+
+		if(player > 2)
+			player = 1;
+	}
+
+	// Resets the game to it's default state
+	public void ResetGame()
+	{
+		// Resets all pieces to default/hidden
+		O_1.SetActive(false);
+		O_2.SetActive(false);
+		O_3.SetActive(false);
+		O_4.SetActive(false);
+		O_5.SetActive(false);
+		O_6.SetActive(false);
+		O_7.SetActive(false);
+		O_8.SetActive(false);
+		O_9.SetActive(false);
+		X_1.SetActive(false);
+		X_2.SetActive(false);
+		X_3.SetActive(false);
+		X_4.SetActive(false);
+		X_5.SetActive(false);
+		X_6.SetActive(false);
+		X_7.SetActive(false);
+		X_8.SetActive(false);
+		X_9.SetActive(false);
+
+		// Set flags to false
+		set1 = false;
+		set2 = false;
+		set3 = false;
+		set4 = false;
+		set5 = false;
+		set6 = false;
+		set7 = false;
+		set8 = false;
+		set9 = false;
+
+		// Choose a random player
+		player = 1;
+	}
+
+	// Places a game token on the specified slot
+	// The GameManager knows which player is going and will place the correct piece on the board
+	public void SetGameToken(int slot)
+	{
+		switch(player)
+		{
+		// If Player 1
+		case 1:
+			if(slot == 1 && !set1)
+			{
+				X_1.SetActive(true);
+				set1 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 2 && !set2)
+			{
+				X_2.SetActive(true);
+				set2 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 3 && !set3)
+			{
+				X_3.SetActive(true);
+				set3 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 4 && !set4)
+			{
+				X_4.SetActive(true);
+				set4 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 5 && !set5)
+			{
+				X_5.SetActive(true);
+				set5 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 6 && !set6)
+			{
+				X_6.SetActive(true);
+				set6 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 7 && !set7)
+			{
+				X_7.SetActive(true);
+				set7 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 8 && !set8)
+			{
+				X_8.SetActive(true);
+				set8 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 9 && !set9)
+			{
+				X_9.SetActive(true);
+				set9 = true;
+				ChangeTurn ();
+			}
+			break;
+			// If Player 2
+		case 2:	
+			if(slot == 1 && !set1)
+			{
+				O_1.SetActive(true);
+				set1 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 2 && !set2)
+			{
+				O_2.SetActive(true);
+				set2 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 3 && !set3)
+			{
+				O_3.SetActive(true);
+				set3 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 4 && !set4)
+			{
+				O_4.SetActive(true);
+				set4 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 5 && !set5)
+			{
+				O_5.SetActive(true);
+				set5 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 6 && !set6)
+			{
+				O_6.SetActive(true);
+				set6 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 7 && !set7)
+			{
+				O_7.SetActive(true);
+				set7 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 8 && !set8)
+			{
+				O_8.SetActive(true);
+				set8 = true;
+				ChangeTurn ();
+			}
+			else if(slot == 9 && !set9)
+			{
+				O_9.SetActive(true);
+				set9 = true;
+				ChangeTurn ();
+			}
+			break;
+		}
+	}
+}
---
+++ AwesomeAR/Assets/ScreenScript.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenScript : MonoBehaviour {
+
+	public Font myFont;
+	bool showLabel;
+
+	public int rotation = 90;
+	Vector2 pivotPoint;
+
+	int box_X = 150;
+	int box_Y = 150;
+	int quit_X = 360;
+
+	int top = 1100;
+	int left = 150;
+	int widthMiddle = 305;
+	int heightMiddle = 1255;
+	int bottom = 1410;
+	int right = 460;
+	int farRight = 615;
+
+	// Use this for initialization
+	void Start () {
+		showLabel = false;
+		pivotPoint = new Vector2(widthMiddle, heightMiddle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI()
+	{
+		GUIUtility.RotateAroundPivot(rotation, pivotPoint);
+
+		GUIStyle myStyle = new GUIStyle(GUI.skin.button);
+		myStyle.font = myFont;
+
+		if(GUI.Button(new Rect(left, top, box_X, box_Y), "1", myStyle))
+		{
+			GameManager.instance.SetGameToken(1);
+		}
+		if(GUI.Button(new Rect(widthMiddle, top, box_X, box_Y), "2", myStyle))
+		{
+			GameManager.instance.SetGameToken(2);
+		}
+		if(GUI.Button(new Rect(right, top, box_X, box_Y), "3", myStyle))
+		{
+			GameManager.instance.SetGameToken(3);
+		}
+		if(GUI.Button(new Rect(left, heightMiddle, box_X, box_Y), "4", myStyle))
+		{
+			GameManager.instance.SetGameToken(4);
+		}
+		if(GUI.Button(new Rect(widthMiddle, heightMiddle, box_X, box_Y), "5", myStyle))
+		{
+			GameManager.instance.SetGameToken(5);
+		}
+		if(GUI.Button(new Rect(right, heightMiddle, box_X, box_Y), "6", myStyle))
+		{
+			GameManager.instance.SetGameToken(6);
+		}
+		if(GUI.Button(new Rect(left, bottom, box_X, box_Y), "7", myStyle))
+		{
+			GameManager.instance.SetGameToken(7);
+		}
+		if(GUI.Button(new Rect(widthMiddle, bottom, box_X, box_Y), "8", myStyle))
+		{
+			GameManager.instance.SetGameToken(8);
+		}
+		if(GUI.Button(new Rect(right, bottom, box_X, box_Y), "9", myStyle))
+		{
+			GameManager.instance.SetGameToken(9);
+		}
+		if(GUI.Button(new Rect(farRight, top, quit_X, box_Y), "Reset", myStyle))
+		{
+			GameManager.instance.ResetGame();
+		}
+		if(GUI.Button(new Rect(farRight, heightMiddle, quit_X, box_Y), "Quit", myStyle))
+		{
+			Application.Quit();
+		}
+	}
+}
