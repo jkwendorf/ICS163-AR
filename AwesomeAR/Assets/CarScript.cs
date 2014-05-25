b3b173f1c20fce35ae801564369c20ec42317226
JayhawkForLife <jayhawkfan999@gmail.com>
2014-05-24 18:24:03 -0700

    Added CarScript

---
+++ AwesomeAR/Assets/CarScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public Transform carMesh;
+	
+	int radius = 20;
+	public float currentAngle = 0f;
+	public float targetAngle = 0f;	
+	float oneDegRad = (3.1415926f / 180f);
+	
+	// Use this for initialization
+	void Start () 
+	{
+		rigidbody.velocity = new Vector3(0, 0, 10);
+		
+		transform.Rotate(Vector3.up, 180);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*
+		if(transform.position.z > 20)
+		{
+		transform.position = new Vector3(transform.position.x, transform.position.y, -20) ;
+		}
+		*/
+		
+		if(currentAngle < 6.28)
+		{
+			currentAngle += oneDegRad;
+		}
+		else
+		{
+			currentAngle = 0;
+		}
+		
+		float xLoc = Mathf.Cos(currentAngle) * radius;
+		float zLoc = Mathf.Sin (currentAngle) * radius;
+		
+		transform.Rotate(Vector3.up, -1);
+		
+		transform.position = new Vector3(xLoc, 0, zLoc);
+	}
+}
